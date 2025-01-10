8486503e8c52e8461ef7b1e8dae4b1fa59e341ea
imanee11 <imaneelouannane@gmail.com>
2025-01-10 11:58:46 +0100

    add image

--- src/constants/image.jsx
+++ src/constants/image.jsx
@@ -1,5 +1,7 @@
 export default{
     insurance : require("../assets/images/Insurance-bro.png"),
+    insurance2 : require("../assets/images/Insurance-bro2.png"),
+
     img4 : require("../assets/images/hero.png"),
     auto : require("../assets/images/Car crash-amico.png"),
     travel : require("../assets/images/Travel insurance-bro.png"),
